d0fcba188cf0ad6cde30f6b18e056d10df2a54fa
Karan <karan3681.be20@chitkara.edu.in>
2023-05-26 04:46:02 +0530

    used appropriate model for different columns

--- API v3.0 (Beta)/Main.py
+++ API v3.0 (Beta)/Main.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import LabelEncoder
 from flask import Flask, request, jsonify, send_file
 from sklearn.ensemble import IsolationForest
 from flask_cors import CORS
+from sklearn.neighbors import LocalOutlierFactor
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -21,7 +22,7 @@ def preprocess_data(df):
     categorical_features = ['Country', 'Region',
                             'City', 'Browser Name and Version', 'Device Type']
     for feature in categorical_features:
-        df[feature+"_code"] = encoder.fit_transform(df[feature])
+        df[feature + "_code"] = encoder.fit_transform(df[feature])
     df['Hours'] = df['Login Timestamp'].apply(give_hours)
     return df
 
@@ -60,7 +61,8 @@ def find_anomalies_in_uid_time(anomalies, df):
                                   'Login Successful': row['Login Successful'],
                                   'reason': 'User Login on unusual time'})
 
-def find_anomalies_in_cluster(anomalies, df, a, b, reason):
+
+def find_anomalies_in_cluster_iforest(anomalies, df, a, b, reason):
     # Create a dictionary where keys are the unique values of
     # column A and values are the corresponding values of column B
     dict = {}
@@ -95,9 +97,51 @@ def find_anomalies_in_cluster(anomalies, df, a, b, reason):
                                   'Hours': row['Hours'],
                                   'reason': reason})
 
+
+def find_anomalies_in_cluster_lof(anomalies, df, a, b, reason):
+    # Create a dictionary where keys are the unique values of
+    # column A and values are the corresponding values of column B
+    dict = {}
+    for _, row in df.iterrows():
+        dict.setdefault(row[a], []).append(row[b])
+
+    # Create a new DataFrame where each row represents a unique value of column A
+    # and the corresponding values of column B are represented as a list
+    new_df = pd.DataFrame(list(dict.items()), columns=[a, b])
+    models = {}
+    for _, row in new_df.iterrows():
+        model = LocalOutlierFactor(n_neighbors=2, contamination=0.05, novelty=True)
+        x = np.array(row[b]).reshape(-1, 1)
+        if len(x) == 1:
+            continue
+        model.fit(x)
+        models[row[a]] = model
+
+    # Predict anomalies in the original DataFrame
+    for _, row in df.iterrows():
+        if row[a] in models:
+            model = models[row[a]]
+            x = np.array(row[b]).reshape(-1, 1)
+            y_pred = model.predict(x)
+            if y_pred == -1:
+                anomalies.append({'Login Timestamp': row['Login Timestamp'],
+                                  'User ID': row['User ID'],
+                                  'IP Address': row['IP Address'],
+                                  'Country': row['Country'],
+                                  'Region': row['Region'],
+                                  'City': row['City'],
+                                  'Browser Name and Version': row['Browser Name and Version'],
+                                  'Device Type': row['Device Type'],
+                                  'Login Successful': row['Login Successful'],
+                                  'Hours': row['Hours'],
+                                  'reason': reason})
+
+
 '''
     This route is currently depricated in this version for development purposes
 '''
+
+
 @app.route('/train_model', methods=['POST'])
 def train_model():
     return jsonify({'message': 'Model trained successfully'})
@@ -115,27 +159,23 @@ def detect_anomalies():
     df = pd.read_csv(file)
     df = df.iloc[:50]  ##remove this
     df = preprocess_data(df)
-    df.loc[0, 'Hours'] = 17.0 ##remove this
+    df.loc[0, 'Hours'] = 17.0  ##remove this
     # for user - time relationship
     anomalies = []
-    # find_anomalies_in_uid_time(anomalies, df)
-    find_anomalies_in_cluster(anomalies, df, 'User ID', 'Hours', "User Login outside usual time")
-    find_anomalies_in_cluster(anomalies, df, 'Country', 'Hours', "Suspicious login time for this country")
-    find_anomalies_in_cluster(anomalies, df, 'Country', 'Hours', "Suspicious login time for this country")
-    
-    '''Isolation forest won't work well for these anomalies switching to LoF ASAP'''
-    
-    find_anomalies_in_cluster(anomalies, df, 'IP Address', 'Country_code', "IP Address doesn't match country")
-    find_anomalies_in_cluster(anomalies, df, 'IP Address', 'Region_code', "Your Location Seems to change")
-    find_anomalies_in_cluster(anomalies, df, 'IP Address', 'City_code', "Your Location Seems to change")
-    find_anomalies_in_cluster(anomalies, df, 'IP Address', 'Login Successful',
-                              "Too many Unsuccessful attempts from network")
-    find_anomalies_in_cluster(anomalies, df, 'Country', 'Browser Name and Version_code',
-                              "Outdated/Suspicious browser")
-    find_anomalies_in_cluster(anomalies, df, 'Country', 'Device Type_code',
-                              "This device is rarely using in this country")
+    find_anomalies_in_cluster_iforest(anomalies, df, 'User ID', 'Hours', "User Login outside usual time")
+    find_anomalies_in_cluster_iforest(anomalies, df, 'Country', 'Hours', "Suspicious login time for this country")
 
-    print(anomalies)
+    find_anomalies_in_cluster_lof(anomalies, df, 'IP Address', 'Country_code', "IP Address doesn't match country")
+    find_anomalies_in_cluster_lof(anomalies, df, 'IP Address', 'Region_code', "Your Location Seems to change")
+    find_anomalies_in_cluster_lof(anomalies, df, 'IP Address', 'City_code', "Your Location Seems to change")
+    find_anomalies_in_cluster_lof(anomalies, df, 'IP Address', 'Login Successful',
+                                  "Too many Unsuccessful attempts from network")
+    find_anomalies_in_cluster_lof(anomalies, df, 'Country', 'Browser Name and Version_code',
+                                  "Outdated/Suspicious browser")
+    find_anomalies_in_cluster_lof(anomalies, df, 'Country', 'Device Type_code',
+                                  "This device is rarely using in this country")
+    for i in anomalies:
+        print(i)
 
     return jsonify({'message': 'Detected'})
 
